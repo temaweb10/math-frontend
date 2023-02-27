15f46539ed65006a14bdf712c369bac65db776e6
temaweb10 <pashkovjob@gmail.com>
2023-02-27 20:31:36 +0300

    keyboard start

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { addStyles, EditableMathField } from "react-mathquill";
 import Header from "../../components/Header/Header";
+addStyles();
 function Main() {
+  const [latex, setLatex] = useState("\\frac{1}{\\sqrt{2}}\\cdot 2");
   return (
     <div>
       <Header />
-      АРТЁМ И ОЛЕГ БЕСТ
+      <div>
+        <EditableMathField
+          latex={latex}
+          onChange={(mathField) => {
+            setLatex(mathField.latex());
+          }}
+        />
+        <p>{latex}</p>
+      </div>
     </div>
   );
 }
